5356f4c325d961c637c5d7cc37b900b3d875bb81
Jaidev P <jaidev246@gmail.com>
2017-09-10 13:44:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,21 @@ button.onclick= function(){
     request.open('GET', 'http://jaidev246.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick =function(){
+    //make rqst to server
+    var names=['name1','name2','name3','name4'];
+    //capture a list of names and render it as a list
+    var list= '';
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + name[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML =list;
+    
+};
