d43da95bbbe578b6beb1dff8b6d7bc52468eded6
Jaidev P <jaidev246@gmail.com>
2017-09-10 14:03:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
 app.get('/submit-name/:name',function(req,res){
     //get name from req obj
     var name = req.params.name;
