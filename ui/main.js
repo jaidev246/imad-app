7bccacb2edd7e307a977c777951bf9827b47f05f
Jaidev P <jaidev246@gmail.com>
2017-09-10 13:47:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick =function(){
     //capture a list of names and render it as a list
     var list= '';
     for(var i=0;i<names.length;i++){
-        list += '<li>' + name[i] + '</li>';
+        list += '<li>' + names[i] + '</li>';
         
     }
     var ul = document.getElementById('namelist');
