596033b51a7d5608470632c8e370a68913ffc200
Jaidev P <jaidev246@gmail.com>
2017-09-21 10:59:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database: 'jaidev246',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.emv.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
